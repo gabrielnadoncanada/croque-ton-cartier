e15a42ef31be3e7e55eac35a17f14b4de4649013
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2019-05-02 17:21:09 -0400

    positionnement du header

--- wp-content/themes/croque-ton-quartier/header.php
+++ wp-content/themes/croque-ton-quartier/header.php
@@ -28,18 +28,9 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'croque-ton-quartier' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="container">
+		
 		<div class="row">
-			<nav id="site-navigation" class="main-navigation col">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'croque-ton-quartier' ); ?></button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</nav><!-- #site-navigation -->
-			<div class="site-branding col">
+            <div class="site-branding col">
 				<?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
@@ -57,8 +48,18 @@
 					<p class="site-description"><?php echo $croque_ton_quartier_description; /* WPCS: xss ok. */ ?></p>
 				<?php endif; ?>
 			</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation col">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'croque-ton-quartier' ); ?></button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+			
 		</div>
-		</div>
+		
 		
 	</header><!-- #masthead -->
 
